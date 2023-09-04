cc09469867fca1b5f4f92c8875bd6ba52ae14650
mark-br-git <115547439+mark-br-git@users.noreply.github.com>
2023-09-04 22:04:05 +0200

    Update semver_script.py

--- semver_script.py
+++ semver_script.py
@@ -3,7 +3,9 @@ import sys
 
 current_version = sys.argv[1]
 parsed_version = semver.VersionInfo.parse(current_version)
-new_version = parsed_version.bump_minor()
+new_version1 = parsed_version.bump_minor()
+new_version2 = new_version1.bump_minor()
+new_version = new_version2.bump_minor()
 
 print(new_version)
 
