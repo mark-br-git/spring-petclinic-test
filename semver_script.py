a316dd0096e2adab74ce693ff3aa4a0bd65a9e2a
mark-br-git <115547439+mark-br-git@users.noreply.github.com>
2023-09-04 14:12:35 +0200

    Update semver_script.py

--- semver_script.py
+++ semver_script.py
@@ -1,24 +1,10 @@
-import os
 import semver
-import subprocess
+import sys
 
-# Odczytaj bieżącą wersję z pliku version.txt
-with open('version.txt', 'r') as version_file:
-    current_version = version_file.read().strip()
-
-# Zwiększ minor version o 1
+current_version = sys.argv[1]
 parsed_version = semver.VersionInfo.parse(current_version)
-new_version = f"{parsed_version.major}.{parsed_version.minor + 1}.{parsed_version.patch}"
+new_version = parsed_version.bump_minor()
 
-# Aktualizuj plik version.txt z nową wersją
-with open('version.txt', 'w') as version_file:
-    version_file.write(new_version)
+print(new_version)
 
-# Tworzenie i zapisywanie taga w repozytorium Git
-tag_name = f"v{new_version}"
-subprocess.run(["git", "tag", "-a", tag_name, "-m", f"Release {tag_name}"])
-subprocess.run(["git", "push", "origin", tag_name])
-
-# Teraz możesz użyć nowej wersji do innych działań w swoim skrypcie
-print(f'Created Git tag: {tag_name}')
 
