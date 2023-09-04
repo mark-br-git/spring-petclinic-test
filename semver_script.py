4d0c31a80bd1aab3e37b67c4d47ba017dc26d701
mark-br-git <115547439+mark-br-git@users.noreply.github.com>
2023-09-04 21:37:37 +0200

    Update semver_script.py

--- semver_script.py
+++ semver_script.py
@@ -1,7 +1,7 @@
 import semver
 import sys
 
-current_version = 1.2.0
+current_version = "1.2.0"
 parsed_version = semver.VersionInfo.parse(current_version)
 new_version = parsed_version.bump_minor()
 
